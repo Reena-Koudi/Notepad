3e20ee6c33e6ed54d0232918df898d1476fa1e59
Reenakoudi <kreena92@gmail.com>
2018-04-05 11:36:35 +0100

    Added test to NoteListView to check for note list model that has no notes, one note

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,15 +1,36 @@
 (function(exports){
   function testNoteListViewMultipleNotes(){
     var noteList = new NoteList();
-    console.log("Hi");
     noteList.createNote("Javascript");
     noteList.createNote("Ruby");
     var noteListView = new NoteListView(noteList);
     htmlView = noteListView.convertToHTMLView();
-    console.log(htmlView);
     output = "<ul><li><div>Javascript</div></li><li><div>Ruby</div></li></ul>";
-    console.log(output);
     assert.isTrue(htmlView === output);
   };
   exports.testNoteListViewMultipleNotes = testNoteListViewMultipleNotes();
+})(this);
+
+
+(function(exports){
+  function testNoteListViewOneNote(){
+    var noteList = new NoteList();
+    noteList.createNote("Javascript");
+    var noteListView = new NoteListView(noteList);
+    htmlView = noteListView.convertToHTMLView();
+    output = "<ul><li><div>Javascript</div></li></ul>";
+    assert.isTrue(htmlView === output);
+    };
+    exports.testNoteListViewOneNote = testNoteListViewOneNote();
+})(this);
+
+(function(exports){
+  function testNoteListViewNoNote(){
+    var noteList = new NoteList();
+    var noteListView = new NoteListView(noteList);
+    htmlView = noteListView.convertToHTMLView();
+    output = "<ul></ul>";
+    assert.isTrue(htmlView === output);
+    };
+    exports.testNoteListViewNoNote = testNoteListViewNoNote();
 })(this);
