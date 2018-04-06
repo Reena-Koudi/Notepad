f449e6f5fffda1b4d278835a518982b29b24e18b
Reenakoudi <kreena92@gmail.com>
2018-04-06 15:11:25 +0100

    Tested for nte controller can be instantiated

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,12 +1,21 @@
-(function(exports){
-  function testNoteControllerInsertHTMLIntoApp(){
-    var noteList = new NoteList();
+
+(function() {
+  function testNoteControllerIsCreated() {
+    var noteList = new NoteList;
     noteList.createNote("Favourite drink: seltzer");
     var noteController = new NoteController(noteList);
-    noteController.insertHTMLIntoApp();
-    var actualHTMLView = document.getElementById('app').innerHTML;
-    var otputHTMLView = "<ul><li><div>Favourite drink: seltzer</div></li></ul>";
-    assert.isEqual(actualHTMLView, outputHTMLView);
+    assert.isTrue(noteController);
   };
-  exports.testNoteControllerInsertHTMLIntoApp = testNoteControllerInsertHTMLIntoApp();
+
+  function testNoteControllerInsertsHTMLIntoApp(){
+    var noteList = new NoteList;
+    var noteController = new NoteController(noteList);
+    noteController.insertHTMLIntoApp();
+    var actualInnerHTML = document.getElementById('app').innerHTML;
+    var expectedInnerHTML = "<ul><li><div>Favourite drink: seltzer</div></li></ul>";
+    assert.isEqual(actualInnerHTML, expectedInnerHTML);
+  };
+
+  testNoteControllerIsCreated();
+  testNoteControllerInsertsHTMLIntoApp()
 })(this);
